d458aa85f204d05b9c2b73aa8a49865a15170226
Navin Tigga <146591653+SchemedCoder@users.noreply.github.com>
2026-06-12 00:12:27 +0530

    Update 06_bronze.sql

--- sql/06_bronze.sql
+++ sql/06_bronze.sql
@@ -9,19 +9,16 @@ USE SCHEMA CLICKSTREAM;
 CREATE OR REPLACE TABLE bronze_clickstream (
 
 event_id STRING,
-
 user_id STRING,
-
 session_id STRING,
 
 page STRING,
-
 event_type STRING,
 
 product_id STRING,
+product_price NUMBER(10,2),
 
 device_type STRING,
-
 country STRING,
 
 event_time TIMESTAMP_NTZ,
