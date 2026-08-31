8772a8ed833edc968b1172374710b445ff26905c
Navin Tigga <146591653+SchemedCoder@users.noreply.github.com>
2026-08-31 23:57:09 +0530

    Update 02_storage_integration.sql

--- sql/02_storage_integration.sql
+++ sql/02_storage_integration.sql
@@ -1,1 +1,26 @@
+-- ==================================================
+-- STORAGE INTEGRATION
+-- ==================================================
+
+CREATE OR REPLACE STORAGE INTEGRATION clickstream_adls_int
+
+TYPE = EXTERNAL_STAGE
+
+STORAGE_PROVIDER = AZURE
+
+ENABLED = TRUE
+
+AZURE_TENANT_ID = '<YOUR_AZURE_TENANT_ID>'
+
+STORAGE_ALLOWED_LOCATIONS = (
+
+'azure://clickstreamstorage.dfs.core.windows.net/clickstream'
+
+);
+
+-- ==================================================
+-- VERIFY
+-- ==================================================
+
+DESC INTEGRATION clickstream_adls_int;
 
