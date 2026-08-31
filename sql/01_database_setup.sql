bce02b391aefadef75b5936684b5106ca5671ab0
Navin Tigga <146591653+SchemedCoder@users.noreply.github.com>
2026-08-31 23:56:33 +0530

    Set up database and schema for Clickstream

--- sql/01_database_setup.sql
+++ sql/01_database_setup.sql
@@ -1,1 +1,41 @@
+-- ==================================================
+-- DATABASE
+-- ==================================================
+
+CREATE DATABASE IF NOT EXISTS CLICKSTREAM_DB;
+
+USE DATABASE CLICKSTREAM_DB;
+
+-- ==================================================
+-- SCHEMA
+-- ==================================================
+
+CREATE SCHEMA IF NOT EXISTS CLICKSTREAM;
+
+USE SCHEMA CLICKSTREAM;
+
+-- ==================================================
+-- WAREHOUSE
+-- ==================================================
+
+CREATE WAREHOUSE IF NOT EXISTS CLICKSTREAM_WH
+
+WITH
+
+WAREHOUSE_SIZE='XSMALL'
+AUTO_SUSPEND=60
+AUTO_RESUME=TRUE
+INITIALLY_SUSPENDED=TRUE;
+
+USE WAREHOUSE CLICKSTREAM_WH;
+
+-- ==================================================
+-- VERIFY
+-- ==================================================
+
+SHOW DATABASES;
+
+SHOW SCHEMAS;
+
+SHOW WAREHOUSES;
 
